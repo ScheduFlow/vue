9172eaa3e52b90e20b5b0e278149416fb62399ed
temp12312yeap <temp12312@naver.com>
2024-02-07 11:17:10 +0900

    App vue 스타일 수정

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,74 @@ import CompleteSchedule from './components/CompleteSchedule.vue'
 import Contributions from './components/Contributions.vue'
 import MemberStatus from './components/MemberStatus.vue'
 import Calendar from './components/Calendar.vue'
+
 </script>
 
 <template>
-	<CompleteSchedule />
-	<div class="calendar-app">
-		<Calendar />
-	</div>
 	
-	<MemberStatus />
-	<Contributions />
+	<div class = "body">
+		<div class="top">
+			<i>ScheduFlow</i>, schedule management team 
+		</div>
+		<div class="sdule">
+			<CompleteSchedule />
+		</div>
+		 <div class="inline">
+			<div class="cal">
+				<Calendar />
+			</div>
+ 		 </div>	
+		<div class ="status">
+		<p>Member Status</p>
+                        <MemberStatus />
+        	</div>
+		
+       
+		 
+		<div class = "contrib">
+			<Contibutions />
+        </div>
+	</div>
+
 </template>
 
-<style lang="css">
+<style scoped>
+	p {
+		font-weight: bold;
+		text-align: center;	
+		padding-bottom: 3px;
+	}	
+	.cal {
+		width: 600px;
+		padding-right : 3px;
+	}
+	.status {
+		width: 500px;
+		height : 500px;
+		border: 1px #9cb dashed;
+		background-color: #9bc;
+		border-radius: 5px;
+	}
+  	.body {
+		margin-top : 0;
+                width:1200px;
+        }
+		
+	.inline {
+		position: relative;
+		display: inline-block;
+		float: left;	
+	}
+     	 .status {
+                float:right;
+        }
+	.top {
+		color:white;	
+	        display:flex;
+                background-color: black;
+                width:100%;
+                margin: 0 auto;
+                padding: 0 20px;
+	}
 
 </style>
